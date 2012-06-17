20b7b1763ccb8af6fabfbc0d4db32267fe3c87de
Neal Lindsay <neal.lindsay@gmail.com>
2012-06-17 09:36:51 -0400

    add a form

--- lib/ga_event_demo.rb
+++ lib/ga_event_demo.rb
@@ -5,8 +5,13 @@ require 'sass'
 class GAEventDemo < Sinatra::Base
 
   get '/' do
-    @title = 'Root Page'
+    @title = 'Form Page'
     haml :index
+  end
+
+  post'/formsubmit' do
+    @title = 'Thank you for submitting'
+    haml :result
   end
 
   get('/main.css') { scss :main }
