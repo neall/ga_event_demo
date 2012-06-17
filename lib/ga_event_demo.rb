50b0852508eff4fcd96e0e607fbd21b32f055d59
Neal Lindsay <neal.lindsay@gmail.com>
2012-06-17 10:25:31 -0400

    added Google Analytics basic tracking code
    because storing credentials in source control is dumb (even credentials
    that are public, like your GA account number), this code now requires an
    environment variable called GA_EVENT_DEMO_GA_ACCOUNT.

--- lib/ga_event_demo.rb
+++ lib/ga_event_demo.rb
@@ -4,6 +4,19 @@ require 'sass'
 
 class GAEventDemo < Sinatra::Base
 
+  helpers do
+    def ga_setup_js
+      "var _gaq = _gaq || [];
+_gaq.push(['_setAccount', '#{ENV['GA_EVENT_DEMO_GA_ACCOUNT']}']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();"
+    end
+  end
+
   get '/' do
     @title = 'Form Page'
     haml :index
